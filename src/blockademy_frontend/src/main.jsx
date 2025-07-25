27e6b28fb05c6923ab94fa778a8c4d90df735d8a
extra785 <extra78578@gmail.com>
2025-07-25 23:21:55 +0300

    feat: updated frontend layout and styles

--- src/blockademy_frontend/src/main.jsx
+++ src/blockademy_frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./App.css";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>
 );
